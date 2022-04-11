285b0274dee958c0b273d8f1e48bb872c63a712d
Yuan Tian <tian@Yuans-Mac-mini.local>
2022-04-12 00:40:07 +0100

    Added champ.Overlap.R

--- README.md
+++ README.md
@@ -23,3 +23,5 @@ During runing ChAMP, some users required some extra features like paired calcula
 **champ.TFEA.R**: A function to do Transcript Factor Binding Site Enrichment Analysis (TFEA) for a list of Region of Interest (ROI), like genes, promoters, differential methylated regions, differential hydroxty-methylated regions .etc.
 
 **champ.PeakEnrich.R**: A function to do peak set enrichment, currently only support MAnorm2 preprocessed ChIP-seq, actually only validated on MeDIP-seq and hMeDIP-seq.
+
+**champ.Overlap.R**: A function to do overlap collapsing between two genome segments(peaks). Overlap x and y, and collapse y records to each matched x record. Functions for collapse can be self-defined.
---
+++ champ.Overlap.R
@@ -0,0 +1,51 @@
+# A script to overlap two Genome-Based segmenets. For example, peaks (CpGs) with genome annotation (CpGIslands, Histone Position)
+# Author: Tian
+
+library("GenomicRanges")
+library("data.table")
+
+# load("../Tmp/Peaks_SubAnno.RData")
+
+ConcatCharacterArray <- function(x) {
+    paste(x, collapse=";")
+}
+
+# x <- peaks
+# y <- subAnno
+# yAttributes=c("feature", "transcriptClass", "geneSymbol")
+# collapseFunctions=c(ConcatCharacterArray, ConcatCharacterArray, ConcatCharacterArray)
+
+champ.Overlap <- function(x,
+                          y,
+                          yAttributes=c("feature", "transcriptClass", "geneSymbol"),
+                          collapseFunctions=list(ConcatCharacterArray, ConcatCharacterArray, ConcatCharacterArray))
+{
+    if(!all(yAttributes %in% colnames(y))) stop("At least one yAttributes does not exist in y data frame.")
+
+    x.gr <- makeGRangesFromDataFrame(x)
+    y.gr <- makeGRangesFromDataFrame(y)
+
+    message("Finding overlap between x and y.")
+    ov <- GenomicRanges::findOverlaps(x.gr, y.gr)
+
+    ovY.dt <- setDT(y[subjectHits(ov), ])
+    ovY.dt[, xIndex := queryHits(ov)]
+    ovY.dt[, yIndex := subjectHits(ov)]
+
+    message("Collapse y for each x.")
+    ovXResult.dt <- ovY.dt[, .(.N, yIndexList = paste(yIndex, collapse=";")), by = .(xIndex)]
+
+    for(i in 1:length(yAttributes)) {
+        message("Collapsing ", yAttributes[i], " ...")
+        ovY.dt[, "tmpColumn" := ovY.dt[, get(yAttributes[i])]]
+        tmp.dt <- ovY.dt[, .(collapseFunctions[[i]](tmpColumn)), by = .(xIndex)]
+        ovXResult.dt <- merge(ovXResult.dt, tmp.dt)
+    }
+
+    message("Prepare data frame for return.")
+    ovXResult <- as.data.frame(ovXResult.dt)
+    colnames(ovXResult) <- c("xIndex", "yOvNumber", "yIndexList", yAttributes)
+    ovXResult <- cbind(x[ovXResult$xIndex, ], ovXResult)
+
+    return(ovXResult)
+}
